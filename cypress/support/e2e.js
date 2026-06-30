ed6988fb8c9e36fff5571edceddb3f3eed85fe97
MarioCarvalho <mario.alves.souza@outlook.com>
2026-06-30 01:31:56 -0300

    Ajuste  Config e e2e

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,5 +1,8 @@
 import './commands';
-
+beforeEach(() => {
+  cy.intercept('**/backtrace.io/**', { statusCode: 200, body: {} });
+  cy.intercept('**/sentry.io/**', { statusCode: 200, body: {} });
+});
 // Oculta exceções de fetch/XHR que não são relevantes para os testes
 Cypress.on('uncaught:exception', (err) => {
   // Ignora erros comuns de aplicações SPA que não afetam os testes
